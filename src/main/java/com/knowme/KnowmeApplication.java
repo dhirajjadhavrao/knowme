1b4db06653ed654e303bb23a2a7841e48e917742
Dhiraj <dhirajjadhavrao@gmail.com>
2022-01-31 19:13:01 +0530

    Initial Setup

---
+++ src/main/java/com/knowme/KnowmeApplication.java
@@ -0,0 +1,13 @@
+package com.knowme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowme/KnowmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class KnowmeApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
