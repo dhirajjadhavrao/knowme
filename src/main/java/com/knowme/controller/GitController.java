37d7549cab3616a67c30734ec4132d00ec85b9de
Dhiraj <dhirajjadhavrao@gmail.com>
2022-02-01 09:50:47 +0530

    GitHub profile details saving logic added

--- src/main/java/com/knowme/controller/GitController.java
+++ src/main/java/com/knowme/controller/GitController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 /**
  * @author Dhiraj Jadhavrao
  * */
@@ -16,7 +18,7 @@ public class GitController {
     private ServiceManager serviceManager;
 
     @GetMapping("/profile")
-    public ResponseEntity getGitProfileDetails(){
+    public ResponseEntity getGitProfileDetails() throws IOException {
         return ResponseEntity.ok(serviceManager.getGitService().getGitProfileDetails());
     }
 
--- src/main/java/com/knowme/repository/GitOwnerDao.java
+++ src/main/java/com/knowme/repository/GitOwnerDao.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface GitOwnerDao extends JpaRepository<GitOwner, Long> {
     @Override
     GitOwner getById(Long id);
-//    GitOwner findByGitId(Long gitId);
+    GitOwner findByGitId(Long gitId);
     GitOwner save(GitOwner gitOwner);
 }
